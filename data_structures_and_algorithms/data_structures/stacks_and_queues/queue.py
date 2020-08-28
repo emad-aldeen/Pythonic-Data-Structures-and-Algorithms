d1cad30a06a468515bb1af9ff7852c0429a3bd15
3madov-77 <emadzxy7@gmail.com>
2020-08-29 00:03:48 +0300

    finshed enqueue methoud

--- data_structures_and_algorithms/data_structures/stacks_and_queues/queue.py
+++ data_structures_and_algorithms/data_structures/stacks_and_queues/queue.py
@@ -5,5 +5,21 @@ class Queue():
         self.front = None
         self.rear = None
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.front == None and self.rear == None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            temp = self.rear
+            self.rear = new_node
+            temp.next = new_node
+
 if __name__ == '__main__':
     queue = Queue()
+    queue.enqueue(5)
+    print(queue.front.value)
+    queue.enqueue(7)
+    # print(queue.rear.value)
+    queue.enqueue(9)
+    print(queue.rear.value)
