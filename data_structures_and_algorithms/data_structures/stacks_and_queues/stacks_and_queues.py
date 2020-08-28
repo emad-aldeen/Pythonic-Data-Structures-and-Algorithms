dfbe23b3a5f5c840c29ffcbe3ab9aa1b653102fc
3madov-77 <emadzxy7@gmail.com>
2020-08-28 22:51:50 +0300

    created the setup

---
+++ data_structures_and_algorithms/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    
+
+
+
+if __name__ == '__main__':
+    stack = Stack()
+
