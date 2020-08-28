da2cc87861c4effb92539cf28d77bb344c52779c
3madov-77 <emadzxy7@gmail.com>
2020-08-28 22:52:17 +0300

    finshed push method

--- data_structures_and_algorithms/data_structures/stacks_and_queues/stacks_and_queues.py
+++ data_structures_and_algorithms/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -8,9 +8,21 @@ class Stack:
     def __init__(self):
         self.top = None
     
+    def push(self, value):
+        new_node = Node(value)
+        if self.top == None:
+            self.top = new_node
+        else:
+            temp = self.top
+            new_node.next = temp
+            self.top = new_node
 
 
 
 if __name__ == '__main__':
     stack = Stack()
-
+    stack.push(7)
+    stack.push(9)
+    stack.push(12)
+    print (stack.top.value)
+    print (stack.top.next.value)
