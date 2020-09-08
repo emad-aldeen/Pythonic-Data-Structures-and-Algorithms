11897f71b93b50a3344060ac3e583b711357e5a7
3madov-77 <emadzxy7@gmail.com>
2020-09-09 01:22:49 +0300

    almost done

---
+++ data_structures_and_algorithms/challenges/fizz_buzz_tree/README.md
@@ -0,0 +1,18 @@
+# Fizz Buzz Tree :
+<br>
+
+
+## Challenge
+Tree one of the most famous data structres in the world .. and there is many ways to implement it..
+and this metohd is writen for finding 'fizz, buzz or fizzbuzz' values in the inputed tree and retruns it as new tree..
+
+
+## Approach & Efficiency
+well for me .. i learned alot that is alot ways to slove proplem but always the best solution is which dosent need alot of time and memory to excute and get you the answer..
+no mater how data you had..or even in worst case it is!!
+
+
+<br>
+
+## Solution
+
--- data_structures_and_algorithms/challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ data_structures_and_algorithms/challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,4 +1,4 @@
-from data_structures_and_algorithms.data_structures.tree.tree import BinaryTree
+from data_structures_and_algorithms.data_structures.tree.tree import BinaryTree, _Node
 
 
 def fizz_buzz(value):
@@ -23,20 +23,24 @@ def fizz_buzz_tree(tree):
         inp ---> tree to be checked..
         out >>> new tree with fizz - buzz or the same number if it not..
     """
-    new_tree = BinaryTree()
-    if not tree.root:
+    try:
+        new_tree = BinaryTree()
+        if not tree.root:
+            return new_tree
+
+        def helper(current):
+            """
+            Helper function to use in recurtion to add new values in the new_tree according to their positions in the original tree
+            """
+            node = _Node(fizz_buzz(current.value))
+            if current.left:
+                node.left = helper(current.left)
+            if current.right:
+                node.right = helper(current.right)
+            return node
+        new_tree.root = helper(tree.root)
+
         return new_tree
 
-    def helper(current):
-        """
-        Helper function to use in recurtion to add new values in the new_tree according to their positions in the original tree
-        """
-        node = _Node(fizz_buzz(current.value))
-        if current.left:
-            node.left = helper(current.left)
-        if current.right:
-            node.right = helper(current.right)
-        return node
-    new_tree.root = helper(tree.root)
-
-    return new_tree
+    except:
+        raise ValueError('your input not tree')
