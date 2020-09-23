a4a0c8a2b65859fd5de49dcbaffc83230e84fad9
3madov-77 <emadzxy7@gmail.com>
2020-09-23 08:25:53 +0300

    almost there

---
+++ data_structures_and_algorithms/challenges/quick_sort/README.md
@@ -0,0 +1,22 @@
+# Quick Sort
+<br>
+<hr>
+
+## Challenge
+Review the given pseudocode, then trace the algorithm by stepping through the process with the provided sample array. Document your explanation by creating a blog article that shows the step-by-step output after each iteration through some sort of visual.
+
+Once you are done with your article, code a working, tested implementation of Quick Sort based on the pseudocode provided.
+
+<br>
+
+## Approach & Efficiency
+Space complexity Big O(log(n))
+Time complexity Big O(n^2)
+
+<br>
+
+## Solution
+
+[Blog](/blog.md)
+
+[Link to Code](/code.py)
---
+++ data_structures_and_algorithms/challenges/quick_sort/code.py
@@ -0,0 +1,36 @@
+def quicksort(arr, left, right):
+    if left < right:
+        position = partion(arr, left, right)
+
+        quicksort(arr, left, position - 1)
+
+        quicksort(arr, position + 1, right)
+
+
+def partion(arr, left, right):
+    pivot = arr[right]
+
+    low = left - 1
+
+    for i in range(left, right):
+        if arr[i] <= pivot:
+            low += 1
+            swap(arr, i, low)
+
+    swap(arr, right, low + 1)
+
+    return low + 1
+
+
+def swap(arr, i, low):
+    temp = None
+    temp = arr[i]
+    arr[i] = arr[low]
+    arr[low] = temp
+
+
+
+# if __name__ == '__main__':
+#     lst = [8,4,23,42,16,15]
+#     quicksort(lst, 0, 5)
+#     print(lst)
