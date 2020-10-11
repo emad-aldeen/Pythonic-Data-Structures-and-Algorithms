bed6a29fe2118c94b7d14cc429421380f2819253
3madov-77 <emadzxy7@gmail.com>
2020-10-11 09:24:33 +0300

    finshed

--- data_structures_and_algorithms/data_structures/graph/graph.py
+++ data_structures_and_algorithms/data_structures/graph/graph.py
@@ -1,4 +1,5 @@
 from data_structures_and_algorithms.data_structures.graph.queue import Queue, Node
+from data_structures_and_algorithms.data_structures.graph.stack import Stack
 
 class Graph:
     '''
@@ -119,3 +120,35 @@ class Graph:
             return (True, f'${tsum}')
         return (False, '$0')
 
+
+    def depth_first(self, starting_vertex):
+        """
+        Method to do depth-first traversal on a graph.
+        Input: starting vertex
+        Output: list of vertices in the depth-first order
+        """
+
+        vertices = []
+        depth = Stack()
+
+        if starting_vertex not in self.adjacency_list:
+            raise ValueError
+        
+        depth.push(starting_vertex)
+
+        while not depth.is_empty():
+           top_vertex = depth.pop()
+           vertices.append(top_vertex.value)
+           top_node_neighbors = self.get_neighbors(top_vertex)
+
+           for neighbor in top_node_neighbors[::-1]:
+               if not neighbor[0].visited:
+                   top_vertex.visited = True
+                   neighbor[0].visited = True
+
+                   depth.push(neighbor[0])
+
+        for node in self.adjacency_list:
+            node.visited = False
+
+        return vertices
